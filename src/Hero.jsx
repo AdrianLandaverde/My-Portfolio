0247913da62527dc8465f9c2940990bcefc1620b
AdrianLandaverde <adrianlandaverden@gmail.com>
2024-04-27 09:11:09 -0600

    :recycle: Refactor Initial Structure

---
+++ src/Hero.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Hero.css'
+
+function Hero() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Adrian Landaverde</h1>
+    </>
+  )
+}
+
+export default Hero
