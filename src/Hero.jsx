e8598c59163507b1bd819e0ecf0e4fd511a127ba
AdrianLandaverde <adrianlandaverden@gmail.com>
2024-04-27 09:19:44 -0600

    :sparkles: Hero Page

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,9 +5,10 @@ function Hero() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="hero">
       <h1>Adrian Landaverde</h1>
-    </>
+      <h2>Data Analyst, Data Scientist, ML Engineer</h2>
+    </div>
   )
 }
 
