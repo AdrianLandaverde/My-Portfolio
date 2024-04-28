9db994e98acbdefbc11f5c0f71d1b40897b78c96
AdrianLandaverde <adrianlandaverden@gmail.com>
2024-04-27 23:16:59 -0600

    💬Change info

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,8 +6,9 @@ function Hero() {
 
   return (
     <div className="hero">
-      <h1>Adrian Landaverde</h1>
-      <h2>Data Analyst, Data Scientist, ML Engineer</h2>
+      <h1>ADRIAN LANDAVERDE</h1>
+      <h2>DATA ANALYST | DATA SCIENTIST | AI ENGINEER</h2>
+      <h3>FULL-STACK DEVELOPER</h3>
     </div>
   )
 }
