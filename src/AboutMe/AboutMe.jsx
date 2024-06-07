9bfe543fcb790c0250d849357a8da85ee0ce487b
AdrianLandaverde <adrianlandaverden@gmail.com>
2024-06-07 08:56:47 -0600

    :speech_balloon: Add more info and CV

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -11,16 +11,18 @@ function AboutMe() {
             <div className='about-me-content'>
                 <h1 className='about-me-title'>Hi, I'm Adrian Landaverde</h1>
                 <h2 className='about-me-subtitle'>I'm a Data Enthusiast with a lot of knowledge in Data Engineering, Data Analysis, Data Science, AI Engineering, and Full-Stack Development</h2>
+                <h2 className='about-me-subtitle'>Passionate about solving data driven problems, interested in sustainability, and continous self-taught learning</h2>
                 <ul className='about-me-features'>
                     <li>B.S. in Data Science and Mathematics Engineer in Tec de Monterrey</li>
                     <li>Winner of multiple Hackathons from Tec de Monterrey, Toronto, Boston, MIT, and Stanford</li>
                     <li>High-endurance sport competitions</li>
+                    <li>Took over 100+ Data Science related courses</li>
                 </ul>
 
 
                 <div className='about-me-social'>
                     <a href="mailto:adrianlandaverden@gmail.com">
-                        <FaEnvelope className='social-icon' id='iconMail'/>
+                        <FaEnvelope className='social-icon' id='iconMail' />
                     </a>
                     <a href="https://www.linkedin.com/in/adrian-landaverde-nava/" target="_blank" rel="noopener noreferrer" >
                         <FaLinkedin className='social-icon' id='iconLinkedin' />
@@ -42,6 +44,9 @@ function AboutMe() {
             </div>
             <div className='about-me-image'>
                 <img src="src/assets/images/about-me/adrian.JPG" alt="Adrian's Profile" />
+                <div className='quote'>
+                    <p>"The obstacle is the Way." - Ryan Holiday</p>
+                </div>
             </div>
         </section>
     );
