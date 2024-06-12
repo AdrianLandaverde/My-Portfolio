5e9912240f4bd8f1b1c5621f001f1330223fb0b8
AdrianLandaverde <adrianlandaverden@gmail.com>
2024-06-12 06:45:32 -0600

    :iphone: Fix responsive in phone

--- src/main.jsx
+++ src/main.jsx
@@ -21,30 +21,38 @@ function App() {
     return () => clearTimeout(timer); // This will clear the timer when the component unmounts
   }, []);
 
+  const [navOpen, setNavOpen] = useState(false);
+
+
   return (
-  <React.StrictMode>
-    {!showSplash && (
-      <nav>
-        <Link activeClass="active" to="about-me-section" spy={true} smooth={true} offset={-70} duration={500}>About Me</Link>
-        <Link activeClass="active" to="Skills-section" spy={true} smooth={true} offset={-70} duration={500}>Skills</Link>
-        <Link activeClass="active" to="experiences-section" spy={true} smooth={true} offset={-70} duration={500}>Experiences</Link>
-        <Link activeClass="active" to="projects-section" spy={true} smooth={true} offset={-70} duration={500}>Projects</Link>
-        <Link activeClass="active" to="courses-section" spy={true} smooth={true} offset={-70} duration={500}>Courses</Link>
-        <Link activeClass="active" to="achievements-section" spy={true} smooth={true} offset={-70} duration={500}>Achievements</Link>
-      </nav>
-    )}
-    {showSplash ? <Hero /> : (
-      <>
-        <AboutMe id="about-me-section" />
-        <Skills id="Skills-section" />
-        <Experiences id="experiences-section" />
-        <Projects id="projects-section" />
-        <Courses id="courses-section" />
-        <Achievements id="achievements-section" />
-      </>
-    )}
-  </React.StrictMode>
-);
+    <React.StrictMode>
+      {!showSplash && (
+        <nav>
+          <button onClick={() => setNavOpen(!navOpen)}>&#9776;</button>
+          {navOpen && (
+            <>
+              <Link activeClass="active" to="about-me-section" spy={true} smooth={true} offset={-70} duration={500}>About Me</Link>
+              <Link activeClass="active" to="Skills-section" spy={true} smooth={true} offset={-70} duration={500}>Skills</Link>
+              <Link activeClass="active" to="experiences-section" spy={true} smooth={true} offset={-70} duration={500}>Experiences</Link>
+              <Link activeClass="active" to="projects-section" spy={true} smooth={true} offset={-70} duration={500}>Projects</Link>
+              <Link activeClass="active" to="courses-section" spy={true} smooth={true} offset={-70} duration={500}>Courses</Link>
+              <Link activeClass="active" to="achievements-section" spy={true} smooth={true} offset={-70} duration={500}>Achievements</Link>
+            </>
+          )}
+        </nav>
+      )}
+      {showSplash ? <Hero /> : (
+        <>
+          <AboutMe id="about-me-section" />
+          <Skills id="Skills-section" />
+          <Experiences id="experiences-section" />
+          <Projects id="projects-section" />
+          <Courses id="courses-section" />
+          <Achievements id="achievements-section" />
+        </>
+      )}
+    </React.StrictMode>
+  );
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
